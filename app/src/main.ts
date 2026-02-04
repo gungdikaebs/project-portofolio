8f1e098a56c18bd8622ab4852a381286cda36d0b
gungdikaebs <gungdika88@gmail.com>
2026-02-04 13:00:50 +0800

    feat: Set up Tailwind CSS for project styling.

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './styles/globals.css'
+import './styles/variables.css' /* Variables first */
+import './styles/globals.css'   /* Tailwind (Preflight + Utilities) */
+import './styles/base.css'      /* Custom Cleanups/Base Styles (after Preflight) */
 import App from './App.vue'
 
 createApp(App).mount('#app')
